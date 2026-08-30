b807fdb7d85061f83f3c69f32fb23d5198098f42
techueku <ekantkumar031@gmail.com>
2026-08-30 08:55:56 +0530

    Update Elasticsearch configuration setup

--- backend/src/config/elasticsearch.ts
+++ backend/src/config/elasticsearch.ts
@@ -1,5 +1,8 @@
 import { Client } from "@elastic/elasticsearch";
 
 export const elasticsearch = new Client({
-  node: process.env.ELASTICSEARCH_URL || "http://localhost:9200",
+  node: process.env.ELASTICSEARCH_URL,
+  auth: {
+    apiKey: process.env.ELASTICSEARCH_API_KEY!,
+  },
 });
